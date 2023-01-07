294f0c118732715fc6dd661935cf1fe0ede53ba7
Gertjan <Benaan@users.noreply.github.com>
2023-01-07 14:20:08 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,19 +1,10 @@
 #define U_TAPPING_TERM 300
 
-
-ZMK_MACRO(arr,
-        bindings = <&kp KP_MINUS &kp GT>;
-)
-ZMK_MACRO(fnArr,
-        bindings = <&kp EQL &kp GT>;
-)
-
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp M,             U_MT(RSHFT, N),    U_MT(RCTRL, E),    U_MT(RALT, I),     U_MT(LGUI, O),     \
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SEMICOLON,     \
-U_NP,              U_NP,              U_LT(LCTRL, ESC),  U_LT(U_NAV, SPC),  U_MT(LSHFT, TAB)  ,U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
-
+U_NP,              U_NP,              U_MT(LCTRL, ESC),  U_LT(U_NAV, SPC),  U_MT(LSHFT, TAB),  U_MT(LCTRL, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 #define MIRYOKU_LAYER_TAP \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
