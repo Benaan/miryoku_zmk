9899200586b0b80ba1bf695e44d80e1d3535405e
Gertjan <Benaan@users.noreply.github.com>
2023-01-07 14:26:15 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,6 +6,12 @@ U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp 
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SEMICOLON,     \
 U_NP,              U_NP,              U_MT(LCTRL, ESC),  U_LT(U_NAV, SPC),  U_MT(LSHFT, TAB),  U_MT(LCTRL, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
+#define MIRYOKU_LAYER_TAP \
+&kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
+&kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SEMICOLON,     \
+U_NP,              U_NP,              U_MT(LCTRL, ESC),  &kp SPC,           U_MT(LSHFT, TAB),  &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
+
 / {
     combos {
         compatible = "zmk,combos";
