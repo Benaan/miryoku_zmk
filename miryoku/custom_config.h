028c4e138c23844613c4a07ed22d34e44632639f
Gertjan <Benaan@users.noreply.github.com>
2023-01-17 19:48:23 +0100

    Update custom_config.h

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,12 +6,12 @@
 #define MIRYOKU_LAYER_TAP \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 &kp A,             &kp R,             &kp S,             &kp T,             &kp G,             &kp M,             &kp N,             &kp E,             &kp I,             &kp O,             \
-&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SEMICOLON,     \
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp K,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
 U_NP,              U_NP,              &kp SPC,           U_MT(LSHFT, TAB),  U_MT(LCTRL, ESC),  &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_NAV \
-&kp LPAR,          &kp RPAR,          &kp LBRC,          &kp RBRC,          &kp DEL,           U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
+&kp LPAR,          &kp RPAR,          &kp RET,           &kp SEMICOLON,     &kp DEL,           U_RDO,             U_PST,             U_CPY,             U_CUT,             U_UND,             \
 &kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp BSPC,          &u_caps_word,      &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp RET,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
