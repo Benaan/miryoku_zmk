e87a9f9269da218722f346e28f8e6d22fd7316ac
Gertjan <mail@gjkromhout.nl>
2023-01-19 22:02:29 +0100

    fix build

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -6,8 +6,8 @@
 #define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp K,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
-&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp M,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              &kp ESC,           U_LT(U_NAV, SPC),  U_LT(U_SYM, RET),  U_LT(U_SYM,  TAB), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+&kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp M,             &kp H,             &kp COMMA,         &kp DOT,           &kp SLASH,         \
+U_NP,              U_NP,              &kp ESC,           U_LT(U_NAV, SPC),  U_LT(U_SYM, RET),  U_LT(U_SYM, TAB),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_TAP \
@@ -18,20 +18,20 @@ U_NP,              U_NP,              &kp SPC,           U_HT(LSHFT, TAB),  U_HT
 
 
 #define MIRYOKU_LAYER_NAV \
-&kp LPAR,          &kp RPAR,          &kp RET,           &kp SEMICOLON,     &kp DEL,           TRNS,              &u_caps_word,      TRNS,              TRNS,              TRNS,              \
-&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp BSPC,          TRNS,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
+&kp LPAR,          &kp RPAR,          &kp RET,           &kp SEMICOLON,     &kp DEL,           U_NA,              &u_caps_word,      U_NA,              U_NA,              U_NA,              \
+&kp LGUI,          &kp LALT,          &kp LCTRL,         &kp LSHFT,         &kp BSPC,          U_NA,              &kp LEFT,          &kp DOWN,          &kp UP,            &kp RIGHT,         \
 U_UND,             U_CUT,             U_CPY,             U_PST,             U_RDO,             &kp INS,           &kp HOME,          &kp PG_DN,         &kp PG_UP,         &kp END,           \
 U_NP,              U_NP,              U_NA,              U_NA,              U_NA,              &kp TAB,           &kp BSPC,          &kp DEL,           U_NP,              U_NP
 
 #define MIRYOKU_LAYER_SYM \
-&kp DLLR,          &kp PRCT,          &kp AMPS,          &kp PIPE,          &kp CRRT,          U_NA,              TRNS,              TRNS,              TRNS,              TRNS,              \
+&kp DLLR,          &kp PRCT,          &kp AMPS,          &kp PIPE,          &kp CRRT,          U_NA,              U_NA,              U_NA,              U_NA,              U_NA,              \
 &kp COLON,         &kp LBRC,          &kp RBRC,          &kp LPAR,          &kp PLUS,          U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
-&kp HASH,          &kp AT,            &kp EXCL,          &kp EQL,           &kp TILDE,         U_NA,              TRNS,              TRNS,              &kp RALT,          U_NA,              \
+&kp HASH,          &kp AT,            &kp EXCL,          &kp EQL,           &kp TILDE,         U_NA,              U_NA,              U_NA,              &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp LPAR,          &kp RPAR,          &kp UNDER,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
 
 
 #define MIRYOKU_LAYER_NUM \
-&kp LBKT,          &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp RBKT,          U_NA,              TRNS,              TRNS,              TRNS,              U_BOOT,            \
+&kp LBKT,          &kp NUM_7,         &kp NUM_8,         &kp NUM_9,         &kp RBKT,          U_NA,              U_NA,              U_NA,              U_NA,              U_BOOT,            \
 &kp SEMI,          &kp NUM_4,         &kp NUM_5,         &kp NUM_6,         &kp ASTRK,         U_NA,              &kp LSHFT,         &kp LCTRL,         &kp LALT,          &kp LGUI,          \
 &kp GRAVE,         &kp NUM_1,         &kp NUM_2,         &kp NUM_3,         &kp BSLH,          U_NA,              &kp C_PP,          &kp C_NEXT,        &kp RALT,          U_NA,              \
 U_NP,              U_NP,              &kp DOT,           &kp NUM_0,         &kp MINUS,         U_NA,              U_NA,              U_NA,              U_NP,              U_NP
