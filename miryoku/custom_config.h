20f6f43423df4b01073e41a0ab75ffa220c9c571
Gertjan <mail@gjkromhout.nl>
2023-01-19 21:54:26 +0100

    fix build

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -3,7 +3,7 @@
 
 #define U_TAPPING_TERM 200
 
-#define MIRYOKU_ALTERNATIVES_BASE_COLEMAKDHK \
+#define MIRYOKU_LAYER_BASE \
 &kp Q,             &kp W,             &kp F,             &kp P,             &kp B,             &kp J,             &kp L,             &kp U,             &kp Y,             &kp SQT,           \
 U_MT(LGUI, A),     U_MT(LALT, R),     U_MT(LCTRL, S),    U_MT(LSHFT, T),    &kp G,             &kp K,             U_MT(LSHFT, N),    U_MT(LCTRL, E),    U_MT(LALT, I),     U_MT(LGUI, O),     \
 &kp Z,             &kp X,             &kp C,             &kp D,             &kp V,             &kp M,             &kp H,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
